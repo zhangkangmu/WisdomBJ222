599a155dc57182f63d8746933ea4062876aa78d0
zhangyuhong <289590351@qq.com>
2019-03-05 13:33:48 +0800

    【引入侧滑面板SlidingMenu开源项目】 	modified:   src/main/java/com/hong/zyh/wisdombj/MainActivity.java 	new file:   src/main/res/layout/left_menu.xml 	modified:   ../settings.gradle 	../library/

--- app/src/main/java/com/hong/zyh/wisdombj/MainActivity.java
+++ app/src/main/java/com/hong/zyh/wisdombj/MainActivity.java
@@ -1,17 +1,27 @@
 package com.hong.zyh.wisdombj;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 
 /**
  * Created by shuaihong on 2019/3/4.
  */
 
-public class MainActivity extends Activity {
+public class MainActivity extends SlidingFragmentActivity {
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //设置左边的布局
+        setBehindContentView(R.layout.left_menu);
+        //获取SlidingMenu方法
+        SlidingMenu slidingMenu = getSlidingMenu();
+        //全屏触摸
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        //屏幕预留200像素宽度,是主屏，不是侧滑的面板
+        slidingMenu.setBehindOffset(500);
     }
 }
